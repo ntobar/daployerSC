a2db6f802546b71e9c8f4417be96f58c28b68d5f
Nicolas Tobar <tobar.n@husky.neu.edu>
2018-12-28 15:39:14 -0500

    going to try to compile existing contract

--- src/main/java/Web3JClient.java
+++ src/main/java/Web3JClient.java
@@ -1,62 +1,117 @@
-//import org.blockchain.model.ethereum.ERC20;
+import java.math.BigDecimal;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.web3j.crypto.Credentials;
-import org.web3j.crypto.ECKeyPair;
-import org.web3j.crypto.WalletFile;
+import org.web3j.crypto.WalletUtils;
 import org.web3j.protocol.Web3j;
-import org.web3j.protocol.Web3jService;
-import org.web3j.protocol.core.DefaultBlockParameterNumber;
-import org.web3j.protocol.core.Request;
 import org.web3j.protocol.core.methods.response.TransactionReceipt;
 import org.web3j.protocol.http.HttpService;
-//import org.web3j.protocol.parity.Parity;
-//import org.web3j.protocol.parity.methods.response.ParityExportAccount;
-//import org.web3j.protocol.parity.methods.response.ParityTracesResponse;
-//import org.web3j.protocol.parity.methods.response.Trace;
-import org.web3j.tx.FastRawTransactionManager;
-import org.web3j.tx.TransactionManager;
+import org.web3j.sample.contracts.generated.Greeter;
+import org.web3j.tx.Transfer;
+import org.web3j.tx.gas.ContractGasProvider;
 import org.web3j.tx.gas.DefaultGasProvider;
-import org.web3j.tx.response.NoOpProcessor;
+import org.web3j.utils.Convert;
+import org.web3j.utils.Numeric;
 
-import java.math.BigInteger;
-import java.util.List;
 
+
+/**
+ * A simple web3j application that demonstrates a number of core features of web3j:
+ *
+ * <ol>
+ *     <li>Connecting to a node on the Ethereum network</li>
+ *     <li>Loading an Ethereum wallet file</li>
+ *     <li>Sending Ether from one address to another</li>
+ *     <li>Deploying a smart contract to the network</li>
+ *     <li>Reading a value from the deployed smart contract</li>
+ *     <li>Updating a value in the deployed smart contract</li>
+ *     <li>Viewing an event logged by the smart contract</li>
+ * </ol>
+ *
+ * <p>To run this demo, you will need to provide:
+ *
+ * <ol>
+ *     <li>Ethereum client (or node) endpoint. The simplest thing to do is
+ *     <a href="https://infura.io/register.html">request a free access token from Infura</a></li>
+ *     <li>A wallet file. This can be generated using the web3j
+ *     <a href="https://docs.web3j.io/command_line.html">command line tools</a></li>
+ *     <li>Some Ether. This can be requested from the
+ *     <a href="https://www.rinkeby.io/#faucet">Rinkeby Faucet</a></li>
+ * </ol>
+ *
+ * <p>For further background information, refer to the project README.
+ */
 public class Web3JClient {
-  private static Web3jService service = new HttpService("http://localhost:8545");
-  Web3j web3 = Web3j.build(new HttpService("https://morden.infura.io/your-token")); //USING INFURA OUR TOKEN
 
+  //org.web3j.codegen.SolidityFunctionWrapperGenerator
+  
 
-  //When WEB3 is no longer needed: web3.shutdown()
+  private static final Logger log = LoggerFactory.getLogger(Web3JClient.class);
 
-  // create modules
-  private static Web3j web3j = Web3j.build(service);
-  //private static Parity parity = Parity.build(service);
   public static void main(String[] args) throws Exception {
-    /**
-     *  TEST DEPLOY AND CALL CONTRACT FUNCTION
-     */
-    // load private key into eckey to sign
-    String privatekey = "***********************************";
-    BigInteger privkey = new BigInteger(privatekey, 16);
-    ECKeyPair ecKeyPair = ECKeyPair.create(privkey);
-    Credentials credentials = Credentials.create(ecKeyPair);
-    NoOpProcessor processor = new NoOpProcessor(web3j);
+    new Web3JClient().run();
+  }
 
-    //deploy new contract
-    TransactionManager txManager = new FastRawTransactionManager(web3j, credentials, processor);
+  private void run() throws Exception {
 
-    //RemoteCall<ERC20> request = ERC20.deploy(web3j, txManager, DefaultGasProvider.GAS_PRICE, DefaultGasProvider.GAS_LIMIT);
-    //ERC20 token = request.send();
-   //String contractAddress = token.getDeployedAddress("3"); // 3 is ropsten testnet
+    // We start by creating a new web3j instance to connect to remote nodes on the network.
+    // Note: if using web3j Android, use Web3jFactory.build(...
+    Web3j web3j = Web3j.build(new HttpService(
+            "https://rinkeby.infura.io/<your token>"));  // FIXME: Enter your Infura token here;
+    log.info("Connected to Ethereum client version: "
+            + web3j.web3ClientVersion().send().getWeb3ClientVersion());
 
-    // load existing contract by address
-    // ERC20 token = ERC20.load(contractAddress, web3j, txManager, DefaultGasProvider.GAS_PRICE, DefaultGasProvider.GAS_LIMIT);
+    // We then need to load our Ethereum wallet file
+    // FIXME: Generate a new wallet file using the web3j command line tools https://docs.web3j.io/command_line.html
+    Credentials credentials =
+            WalletUtils.loadCredentials(
+                    "<password>",
+                    "/path/to/<walletfile>");
+    log.info("Credentials loaded");
 
+    // FIXME: Request some Ether for the Rinkeby test network at https://www.rinkeby.io/#faucet
+    log.info("Sending 1 Wei ("
+            + Convert.fromWei("1", Convert.Unit.ETHER).toPlainString() + " Ether)");
+    TransactionReceipt transferReceipt = Transfer.sendFunds(
+            web3j, credentials,
+            "0x19e03255f667bdfd50a32722df860b1eeaf4d635",  // you can put any address here
+            BigDecimal.ONE, Convert.Unit.WEI)  // 1 wei = 10^-18 Ether
+            .send();
+    log.info("Transaction complete, view it at https://rinkeby.etherscan.io/tx/"
+            + transferReceipt.getTransactionHash());
 
-    // create transaction transfer token to receiver
-    String receiver = "0xa107483c8a16a58871182a48d4ba1fbbb6a*****";
-    BigInteger value = new BigInteger("10000000000000");
-    //TransactionReceipt receipt = token.transfer(receiver, value).send();
-    // get transaction result
-    //System.out.println(receipt.getTransactionHash());
+    // Now lets deploy a smart contract
+    log.info("Deploying smart contract");
+    ContractGasProvider contractGasProvider = new DefaultGasProvider();
+    Greeter contract = Greeter.deploy(
+            web3j,
+            credentials,
+            contractGasProvider,
+            "test"
+    ).send();
+
+    String contractAddress = contract.getContractAddress();
+    log.info("Smart contract deployed to address " + contractAddress);
+    log.info("View contract at https://rinkeby.etherscan.io/address/" + contractAddress);
+
+    log.info("Value stored in remote smart contract: " + contract.greet().send());
+
+    // Lets modify the value in our smart contract
+    TransactionReceipt transactionReceipt = contract.newGreeting("Well hello again").send();
+
+    log.info("New value stored in remote smart contract: " + contract.greet().send());
+
+    // Events enable us to log specific events happening during the execution of our smart
+    // contract to the blockchain. Index events cannot be logged in their entirety.
+    // For Strings and arrays, the hash of values is provided, not the original value.
+    // For further information, refer to https://docs.web3j.io/filters.html#filters-and-events
+    for (Greeter.ModifiedEventResponse event : contract.getModifiedEvents(transactionReceipt)) {
+      log.info("Modify event fired, previous value: " + event.oldGreeting
+              + ", new value: " + event.newGreeting);
+      log.info("Indexed event previous value: " + Numeric.toHexString(event.oldGreetingIdx)
+              + ", new value: " + Numeric.toHexString(event.newGreetingIdx));
+    }
   }
 }
